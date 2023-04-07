a6994ee30fdc29f045605d00ee34cd629e65bfc7
Grimm <94477820+ItzGrimmDev@users.noreply.github.com>
2023-04-08 01:04:44 +0200

    Add files via upload

---
+++ src/commands/everyone/site.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'site',
+  description: 'Affiche le site de Talia',
+  execute(message, args) {
+    const embed = new MessageEmbed()
+      .setColor('#e67e22')
+      .setTitle('Talia • Boutique')
+      .setDescription('Le site de **Talia** est https://taliamc.fr/\nN\'hésites pas à aller y faire un tour !')
+      .setThumbnail('https://cdn.discordapp.com/attachments/1093653326240948257/1094003792359469186/f1c9db6437f02c913bd10bf9df87f337.png')
+      .setFooter('© Talia - 2023', 'https://cdn.discordapp.com/attachments/1093653326240948257/1094003792359469186/f1c9db6437f02c913bd10bf9df87f337.png')
+      .setTimestamp();
+    
+    message.channel.send({ embeds: [embed] });
+  },
+};
