977085096e12bacc021456d47ed8b4283842d3bc
Grimm <94477820+ItzGrimmDev@users.noreply.github.com>
2023-04-08 01:18:55 +0200

    Update boutique.js

--- src/commands/everyone/boutique.js
+++ src/commands/everyone/boutique.js
@@ -2,20 +2,20 @@ const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
 
 module.exports = {
   name: 'boutique',
-  description: 'Affiche les informations sur la boutique de Talia',
+  description: 'Affiche les informations sur la boutique',
   execute(message, args) {
     const embed = new MessageEmbed()
       .setColor('#e67e22')
-      .setTitle('Talia • Boutique')
-      .setDescription('La boutique de **Talia** est https://taliamc.fr/shop\nAchètes sur la boutique afin de te procurer des **avantages** !')
-      .setThumbnail('https://cdn.discordapp.com/attachments/1093653326240948257/1094003792359469186/f1c9db6437f02c913bd10bf9df87f337.png')
-      .setFooter('© Talia - 2023', 'https://cdn.discordapp.com/attachments/1093653326240948257/1094003792359469186/f1c9db6437f02c913bd10bf9df87f337.png')
+      .setTitle('Grimm • Boutique')
+      .setDescription('La boutique de **Grimm** est [Le lien de votre boutique](https://discord.com/channels/@me/117381264826302464)\nAchètes sur la boutique afin de te procurer des **avantages** !')
+      .setThumbnail('https://cdn.discordapp.com/attachments/1093653326240948257/1094038456210034738/4601_github.png')
+      .setFooter('© Grimm - 2023', 'https://cdn.discordapp.com/attachments/1093653326240948257/1094038456210034738/4601_github.png')
       .setTimestamp();
     
     const linkButton = new MessageButton()
       .setLabel('Visiter la boutique')
       .setStyle('LINK')
-      .setURL('https://taliamc.fr/shop')
+      .setURL('https://discord.com/channels/@me/117381264826302464')
       .setEmoji('💸');
     
     const actionRow = new MessageActionRow().addComponents(linkButton);
