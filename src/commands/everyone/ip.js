2ddf5ff96cab8a8b53e2938583fed8097f2601dd
Grimm <94477820+ItzGrimmDev@users.noreply.github.com>
2023-04-08 01:19:39 +0200

    Update ip.js

--- src/commands/everyone/ip.js
+++ src/commands/everyone/ip.js
@@ -6,10 +6,10 @@ module.exports = {
   execute(message, args) {
     const embed = new MessageEmbed()
       .setColor('#e67e22')
-      .setTitle('Talia • Adresse IP')
-      .setDescription('L\'ip du serveur est **play.taliamc.fr**\nEn espèrant te voir bientôt en jeu !')
-      .setThumbnail('https://cdn.discordapp.com/attachments/1093653326240948257/1094003792359469186/f1c9db6437f02c913bd10bf9df87f337.png')
-      .setFooter('© Talia - 2023', 'https://cdn.discordapp.com/attachments/1093653326240948257/1094003792359469186/f1c9db6437f02c913bd10bf9df87f337.png')
+      .setTitle('Grimm • Adresse IP')
+      .setDescription('L\'ip du serveur est **play.grimm.fr**\nEn espèrant te voir bientôt en jeu !')
+      .setThumbnail('https://cdn.discordapp.com/attachments/1093653326240948257/1094038456210034738/4601_github.png')
+      .setFooter('© Grimm - 2023', 'https://cdn.discordapp.com/attachments/1093653326240948257/1094038456210034738/4601_github.png')
       .setTimestamp();
     
     message.channel.send({ embeds: [embed] });
