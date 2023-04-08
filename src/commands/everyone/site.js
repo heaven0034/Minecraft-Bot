d74496c423a491d2b11eb80d5065a898beefe6e0
Grimm <94477820+ItzGrimmDev@users.noreply.github.com>
2023-04-08 15:08:43 +0200

    Update site.js

--- src/commands/everyone/site.js
+++ src/commands/everyone/site.js
@@ -2,23 +2,24 @@ const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
 
 module.exports = {
   name: 'site',
-  description: 'Affiche le site du serveur',
+  description: 'Affiche le site de Irania',
   execute(message, args) {
     const embed = new MessageEmbed()
       .setColor('#e67e22')
-      .setTitle('Grimm • Boutique')
-      .setDescription('Le site de **Grimm** est https://grimm.fr/\nN\'hésites pas à aller y faire un tour !')
-      .setThumbnail('https://cdn.discordapp.com/attachments/1093653326240948257/1094038456210034738/4601_github.png')
-      .setFooter('© Grimm - 2023', 'https://cdn.discordapp.com/attachments/1093653326240948257/1094038456210034738/4601_github.png')
+      .setTitle('Irania • Site')
+      .setDescription('Le site de **Irania** est https://iraniamc.fr/\nN\'hésites pas à aller y faire un tour !')
+      .setThumbnail('https://cdn.discordapp.com/attachments/1094041372262138027/1094224117567332402/server-icon.png')
+      .setFooter('© Irania - 2023', 'https://cdn.discordapp.com/attachments/1094041372262138027/1094224117567332402/server-icon.png')
       .setTimestamp();
-    
+
     const linkButton = new MessageButton()
       .setLabel('Visiter le site')
       .setStyle('LINK')
-      .setURL('https://discord.com/channels/@me/1094032545508765756')
+      .setURL('https://iraniamc.fr')
       .setEmoji('🌐');
-    
+
     const actionRow = new MessageActionRow().addComponents(linkButton);
-    message.channel.send({ embeds: [embed], components: [actionRow] });
+    
+    message.reply({ embeds: [embed], components: [actionRow] });
   },
 };
