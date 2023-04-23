64fdfb6fe6dc438c13c3c54135d76ed235bcd8da
Grimm <94477820+ItzGrimmDev@users.noreply.github.com>
2023-04-24 00:11:05 +0200

    Update help.js

--- src/commands/everyone/help.js
+++ src/commands/everyone/help.js
@@ -12,7 +12,7 @@ module.exports = {
       .setDescription(`Voici la liste des commandes disponibles. Le préfixe est \`${prefix}\`.`)
       .addFields(
         { name: `${emojis.clemolette} Administration - (3)`, value: '\`link\`, \`rules\`, \`verif\`' },
-        { name: `${emojis.note} Everyone - (5)`, value: '\`boutique\`, \`help\`, \`ip\`, \`ping\`, \`site\`' },
+        { name: `${emojis.note} Everyone - (5)`, value: '\`boutique\`, \`help\`, \`invites-lb\`, \`invites\`, \`ip\`, \`ping\`, \`site\`' },
       )
       .setThumbnail('https://cdn.discordapp.com/attachments/1094041372262138027/1094224117567332402/server-icon.png')
       .setFooter('© Irania - 2023', 'https://cdn.discordapp.com/attachments/1094041372262138027/1094224117567332402/server-icon.png')
