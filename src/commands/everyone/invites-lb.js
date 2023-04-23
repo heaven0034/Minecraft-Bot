4cfc516a11bb32ae4127fde4d5d0769be54d009e
Grimm <94477820+ItzGrimmDev@users.noreply.github.com>
2023-04-24 00:13:12 +0200

    Update invites-lb.js

--- src/commands/everyone/invites-lb.js
+++ src/commands/everyone/invites-lb.js
@@ -14,7 +14,7 @@ module.exports = {
       .setTitle("Classement des invitations")
       .setDescription(
         invites
-          .map((invite, index) => `${emojis[index]} - ${invite.inviter.tag.replace(/:/g, ": ")} : ${invite.uses} invitations`)
+          .map((invite, index) => `${emojis[index]} - ${invite.inviter.tag.replace(/:/g, ": ")} : ${invite.uses} invitations !`)
           .join("\n")
       )
       .setThumbnail('https://cdn.discordapp.com/attachments/1094041372262138027/1094224117567332402/server-icon.png')
