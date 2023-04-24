2b185192efdaa3f83e7f4ac0336a9940101ebb12
Grimm <94477820+ItzGrimmDev@users.noreply.github.com>
2023-04-24 15:01:32 +0200

    Update help.js

--- src/commands/everyone/help.js
+++ src/commands/everyone/help.js
@@ -6,9 +6,11 @@ module.exports = {
   name: 'help',
   description: 'Affiche la liste des commandes',
   execute(message, args) {
+    const serveur = 'https://discord.gg/zkt4yP457D'
     const embed = new MessageEmbed()
       .setColor('#e67e22')
       .setTitle('Commandes disponibles:')
+      .setURL(serveur)
       .setDescription(`Voici la liste des commandes disponibles. Le préfixe est \`${prefix}\`.`)
       .addFields(
         { name: `${emojis.clemolette} Administration - (3)`, value: '\`link\`, \`rules\`, \`verif\`' },
