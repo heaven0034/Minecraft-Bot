b2947e00984ad3280fecfa264e5a2180fbd7fb66
Grimm <94477820+ItzGrimmDev@users.noreply.github.com>
2023-04-24 15:01:50 +0200

    Update boutique.js

--- src/commands/everyone/boutique.js
+++ src/commands/everyone/boutique.js
@@ -4,9 +4,11 @@ module.exports = {
   name: 'boutique',
   description: 'Affiche les informations sur la boutique de Irania',
   execute(message, args) {
+    const serveur = 'https://discord.gg/zkt4yP457D'
     const embed = new MessageEmbed()
       .setColor('#e67e22')
       .setTitle('Irania • Boutique')
+      .setURL(serveur)
       .setDescription('La boutique de **Irania** est https://iraniamc.fr/shop\nAchètes sur la boutique afin de te procurer des **avantages** !')
       .setThumbnail('https://cdn.discordapp.com/attachments/1094041372262138027/1094224117567332402/server-icon.png')
       .setFooter('© Irania - 2023', 'https://cdn.discordapp.com/attachments/1094041372262138027/1094224117567332402/server-icon.png')
